cde85d722835e2f4c02959c514a3574465dae80a
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-01-22 21:55:29 +0200

    Add ECDSA digital signature validation for transactions

--- src/bin.rs
+++ src/bin.rs
@@ -1,17 +1,43 @@
 use hex;
 use blockchain::{Blockchain, Transaction};
 use serde_json;
+use std::fs;
+use rand_core::OsRng;
+use k256::{
+    ecdsa::{SigningKey, Signature, signature::{Signer, Verifier}, VerifyingKey}
+};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut blockchain = Blockchain::new();
 
-    let transaction = Transaction::new("Vlad Slivilescu".to_string(), "Ionut Popescu".to_string(), 550);
+    let sender_signing_key = SigningKey::random(&mut OsRng);
+    let receiver_signing_key = SigningKey::random(&mut OsRng);
+
+    let transaction = Transaction::new(sender_signing_key.clone(), sender_signing_key.verifying_key(), receiver_signing_key.verifying_key(), 1000);
     blockchain.add_transaction(transaction);
-    blockchain.generate_block();
+    blockchain.mine_block().await;
 
     let first_block = blockchain.get_block(0).unwrap();
+    let first_block_json = serde_json::to_string_pretty(first_block).unwrap();
+    println!("First block: ");
+    println!("{}", first_block_json);
+    println!();
+
+    println!("Second block: ");
     let second_block = blockchain.get_block(1).unwrap();
-    let json = serde_json::to_string_pretty(second_block).unwrap();
-    println!("{}", json);
-    println!("{}", hex::encode(first_block.hash()));
+    let second_block_json = serde_json::to_string_pretty(second_block).unwrap();
+    println!("{}", second_block_json);
+    println!();
+
+    println!("Does the previous block hash of the second block match the hash of the first block?");
+    println!("{} == {}", hex::encode(second_block.previous_hash), hex::encode(first_block.hash()));
+
+    println!("Have all transactions in the second block been signed correctly using ECDSA?");
+    if second_block.verify() {
+        println!("Yes!");
+    }
+    else {
+        println!("No!");
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,80 @@
 use hex;
+use std::convert::TryInto;
 use sha2::{Sha256, Digest};
 use chrono::{DateTime, Local};
 use serde::{Serialize, Deserialize, Serializer};
 use serde::ser::{SerializeStruct};
 use bincode;
+use rand::Rng;
+use k256::{
+    ecdsa::{SigningKey, Signature, signature::{Signer, Verifier}, VerifyingKey}
+};
 
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Transaction {
-    sender: String,
-    recipient: String,
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UnsignedTransaction {
+    sender: VerifyingKey,
+    receiver: VerifyingKey,
     amount: i128,
+    timestamp: DateTime<Local>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Transaction {
+    sender: VerifyingKey,
+    receiver: VerifyingKey,
+    amount: i128,
+    timestamp: DateTime<Local>,
+    signature: Signature,
 }
 
 impl Transaction {
-    pub fn new(sender: String, recipient: String, amount: i128) -> Transaction {
+    pub fn new(private_key: SigningKey, public_key: VerifyingKey, receiver: VerifyingKey, amount: i128) -> Transaction {
+        let timestamp = Local::now();
+
+        let transaction = UnsignedTransaction {
+            sender: public_key.clone(),
+            receiver: receiver.clone(),
+            amount,
+            timestamp,
+        };
+
+        let serialized = bincode::serialize(&transaction).unwrap();
+        let signature = private_key.sign(&serialized[..]);
+
         Transaction {
-            sender,
-            recipient,
-            amount
+            sender: public_key,
+            receiver,
+            amount,
+            timestamp,
+            signature,
+        }
+    }
+
+    pub fn verify(&self) -> bool {
+        let transaction = UnsignedTransaction {
+            sender: self.sender.clone(),
+            receiver: self.receiver.clone(),
+            amount: self.amount.clone(),
+            timestamp: self.timestamp.clone(),
+        };
+
+        let verifying_key = VerifyingKey::from(transaction.sender.clone());
+        let serialized = bincode::serialize(&transaction).unwrap();
+        match verifying_key.verify(&serialized[..], &self.signature) {
+            Ok(_) => true,
+            Err(_) => false,
         }
     }
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Block {
-    index: usize,
+    height: usize,
     timestamp: DateTime<Local>,
     transactions: Vec<Transaction>,
-    previous_hash: [u8; 32],
+    pub previous_hash: [u8; 32],
+    target: [u8; 32],
+    nonce: u32,
 }
 
 impl Serialize for Block {
@@ -37,9 +83,10 @@ impl Serialize for Block {
         S: Serializer,
     {
         // 3 is the number of fields in the struct.
-        let mut state = serializer.serialize_struct("Color", 3)?;
-        state.serialize_field("index", &self.index)?;
+        let mut state = serializer.serialize_struct("Color", 5)?;
+        state.serialize_field("index", &self.height)?;
         state.serialize_field("timestamp", &self.timestamp)?;
+        state.serialize_field("nonce", &self.nonce)?;
         state.serialize_field("transactions", &self.transactions)?;
         state.serialize_field("previous_hash", &hex::encode(self.previous_hash))?;
         state.end()
@@ -47,12 +94,15 @@ impl Serialize for Block {
 }
 
 impl Block {
-    pub fn new(index: usize, previous_hash: [u8; 32]) -> Block {
+    pub fn new(height: usize, previous_hash: [u8; 32], target: [u8; 32]) -> Block {
+        let mut rng = rand::thread_rng();
         Block {
-            index,
+            height,
             timestamp: Local::now(),
             transactions: Vec::<Transaction>::new(),
+            target,
             previous_hash,
+            nonce: rng.gen(),
         }
     }
 
@@ -66,13 +116,26 @@ impl Block {
         self.transactions.push(transaction);
     }
 
-}
+    pub fn is_valid(&self) -> bool {
+        hex::encode(self.hash()).cmp(&hex::encode(&self.target)) == std::cmp::Ordering::Less
+    }
 
+    pub fn verify(&self) -> bool {
+        for transaction in self.transactions.iter() {
+            if !transaction.verify() {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Blockchain {
     chain: Vec<Block>,
     pending_transactions: Vec<Transaction>,
+    target: [u8; 32],
 }
 
 impl Blockchain {
@@ -80,25 +143,36 @@ impl Blockchain {
         let mut blockchain = Blockchain {
             chain: Vec::<Block>::new(),
             pending_transactions: Vec::<Transaction>::new(),
+            target: hex::decode("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff").unwrap().try_into().unwrap(),
         };
         blockchain.generate_block();
         blockchain
     }
     
-    pub fn generate_block(&mut self) {
-        let index = self.chain.len();
-        let previous_block = if index > 0 { self.chain.get(index - 1) } else { None };
+    fn generate_block(&mut self) {
+        let height = self.chain.len();
+        let previous_block = if height > 0 { self.chain.get(height - 1) } else { None };
 
         let previous_hash = match previous_block {
             None => [0; 32],
             Some(block) => block.hash(),
         };
 
-        let mut new_block = Block::new(index, previous_hash); 
-        while let Some(transaction) = self.pending_transactions.pop() {
-            new_block.add_transaction(transaction);
+        loop {
+            let mut new_block = Block::new(height, previous_hash, self.target.clone()); 
+            for transaction in self.pending_transactions.iter() {
+                new_block.add_transaction(transaction.clone());
+            }
+            if new_block.is_valid() { 
+                self.chain.push(new_block);
+                self.pending_transactions = Vec::new();
+                break; 
+            }
         }
-        self.chain.push(new_block);
+    }
+
+    pub async fn mine_block(&mut self) {
+        self.generate_block();
     }
 
     pub fn get_last_block(&self) -> Option<&Block> {
